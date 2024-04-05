756a945e25a3e5e236b52b5eb2a8143ed17ab599
Anar <153716590+Zero2220@users.noreply.github.com>
2024-04-05 20:04:46 +0400

    Add files via upload

---
+++ task/ConsoleWeb/ConsoleWeb/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleWeb.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWeb
+{
+    internal class AppDbContext:DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-J30FIEB\\SQLEXPRESS;Database=webdb1;Trusted_connection=true");
+        }
+    }
+}
